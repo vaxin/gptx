2eb020363641f8c39323d386319bfb33119ce2d8
dengfanxin.dfx <dengfanxin.dfx@bytedance.com>
2022-03-13 13:52:01 +0800

    add addition

---
+++ addition.py
@@ -0,0 +1,30 @@
+n = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+m = {}
+for i in range(0, len(n) - 1):
+  m[n[i]] = n[i+1]
+
+def next(x):
+  if x not in m:
+    return None
+  else:
+    return m[x]
+
+def end(x):
+  return x is None
+
+def count(x):
+  p, v1 = int(x / 10), x % 10
+  v2 = next(v1)
+  if end(v2):
+    count(p)
+  else:
+    x = p * 10 + v2
+
+  return x
+
+x = 0
+
+for i in range(100):
+  x = count(x)
+  print(x)
